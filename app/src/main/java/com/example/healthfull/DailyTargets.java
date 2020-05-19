e45368d6308f9cc53150f57853bb548db7e3dea0
fpn1785 <64460435+fpn1785@users.noreply.github.com>
2020-05-19 20:48:43 +1200

    Daily targets

---
+++ app/src/main/java/com/example/healthfull/DailyTargets.java
@@ -0,0 +1,60 @@
+package com.example.healthfull;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DailyTargets {
+
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+    public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            Button addbutton = (Button) findViewById(R.id.addbutton);
+            addbutton.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                    EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+
+
+                    int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                    int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+
+
+
+
+
+                }
+
+            });
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
