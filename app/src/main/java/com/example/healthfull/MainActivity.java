343bb2c3d324c305d4a9a661d3ae82e411aa90a8
Max Troughear <max.troughear@gmail.com>
2020-05-24 22:14:25 +1200

    Fixed login screen keeping history

--- app/src/main/java/com/example/healthfull/MainActivity.java
+++ app/src/main/java/com/example/healthfull/MainActivity.java
@@ -95,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
             public boolean onMenuItemClick(MenuItem item) {
                 FirebaseAuth.getInstance().signOut();
                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
                 return true;
             }
